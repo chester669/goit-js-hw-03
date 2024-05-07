b5abe4ea5b2ec0bde8ad19bae818c9be4ebbfc8a
chester669 <viktorostash44@gmail.com>
2024-05-07 15:42:52 +0300

    homework done

---
+++ js/task-3.js
@@ -0,0 +1,14 @@
+function filterArray(numbers, value) {
+  const highNumbersArray = [];
+  for (const higherNumber of numbers) {
+    if (higherNumber > value) {
+      highNumbersArray.push(higherNumber);
+    }
+  }
+  return higherNumberArray;
+}
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
